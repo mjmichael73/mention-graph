0d4e712c5161ae4e21c6333fec18baa9c450d8e4
Mojtaba Michael <mojimich2015@gmail.com>
2025-07-28 11:48:45 +0300

    feat: Added connection pooling to database

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,32 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+import logging
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:password@mentions-graph-db:5432/mention_graph_db")
 
-engine = create_engine(DATABASE_URL, future=True, echo=False)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:password@mentions-graph-db:5432/mention_graph_db",
+)
+
+engine = create_engine(
+    DATABASE_URL,
+    future=True,
+    echo=False,
+    pool_size=1000,  # Number of connections to keep open (Adjust it based on needs)
+    max_overflow=10,  # Number of connections to allow beyond pool_size
+    pool_timeout=30,  # Timeout for getting a connection from the pool
+)
+
+# Log pool configuration
+logger.info(
+    f"SQLAlchemy Engine created with pool_size={engine.pool.size()}, "
+    f"max_overflow={engine.pool._max_overflow}, "
+    f"pool_timeout={engine.pool._timeout}"
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
